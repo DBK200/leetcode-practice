3c53e7b1912473de19a2c48911aa84bbc30b074e
Claudiu Gurita <claudiu.gurita@cgm.com>
2024-01-07 17:07:03 +0200

    O(n) exercise

--- src/main/java/org/example/exercises/BigOn.java
+++ src/main/java/org/example/exercises/BigOn.java
@@ -1,4 +1,19 @@
 package org.example.exercises;
 
 public class BigOn {
+
+    public static void printItems(int n) {
+        // WRITE THE FOR LOOP TO COMPLETE PRINTITEMS HERE //
+
+        for (int counter = 0; counter < n; counter++) {
+            System.out.println(counter);
+        }
+
+        ////////////////////////////////////////////////////
+    }
+
+    // DO NOT CHANGE THE MAIN METHOD BELOW
+    public static void main(String[] args) {
+        printItems(10);
+    }
 }
